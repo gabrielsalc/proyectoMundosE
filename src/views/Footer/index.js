f4a3754712376cab3926829e0dbec5f45f031f76
gabrielsalc <gabrielsalcedo.gs@gmail.com>
2022-06-07 18:54:34 -0300

    ultimo antes de la muestra

--- src/views/Footer/index.js
+++ src/views/Footer/index.js
@@ -3,7 +3,7 @@ import Logo from './top-logo.png';
 function Footer(){
     return(
     <div ClassName="container-fluid flex flex-box" id="footer">
-    <h4 id="footerh4" >2020 © All rights reserved.</h4>
+    <p id="footerh4" >2020 © All rights reserved.</p>
     <a id="afooter" href="#home"><img src={Logo} alt="logo 25Watts" /></a>
     </div>
     )
